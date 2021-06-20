2a3179d76ba99bbc21bdccd1ab456bfd695eb0dd
Kim Greenbush <kim_greenbush@icloud.com>
2021-06-20 00:30:02 -0700

    define app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from './pokemon.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-mini-project';
+export class AppComponent implements OnInit {
+  title: string
+  tableHead: string[]
+  tableCell: object[]
+
+  constructor(private _service: PokemonService) {
+    this.title = 'Angular Mini Project'
+    this.tableHead = ["Name", "Number", "Type(s)"]
+    this.tableCell = [{}]
+  }
+  ngOnInit() {
+    this.getPokemonFromService()
+  }
+
+  getPokemonFromService() {
+    let pokemon = this._service.getPokemon()
+    this.tableCell = pokemon
+  }
 }
