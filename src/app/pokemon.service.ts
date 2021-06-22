5a6f9df128e530e4dab60ba15b0141eaf05d3c09
Kim Greenbush <kim_greenbush@icloud.com>
2021-06-22 12:22:43 -0700

    add error handler to service

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Observable, throwError } from 'rxjs'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { throwError } from 'rxjs'
 import { catchError, retry } from 'rxjs/operators'
+import {Pokemon } from "./pokemon.model"
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,24 @@ export class PokemonService {
 
   constructor(private _http: HttpClient) { }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error("Error occurred", error.error)
+    } else {
+      console.error("Status: ", error.status, " Error: ", error.error)
+    }
+    return throwError("Try again.")
+  }
+
   getPokemon() {
-    let pokemon: Array<aPokemon> = []
+    const pokemon: Pokemon[] = []
     for (let p = 1; p <= 151; p++) {
-      // I needed to subscribe in service rather than hand back the observable because in order to get multiple pokemon with their properties, I needed to make multiple requests and format them before returning them to the component.
-      this._http.get<aPokemon>(`https://pokeapi.co/api/v2/pokemon/${p}`)
+      // I needed to subscribe in the service rather than hand back the observable because in order to get multiple pokemon with their properties, I needed to make multiple requests and format them before returning a single collection to the component.
+      const subscription = this._http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${p}`)
+        .pipe(
+          retry(1),
+          catchError(this.handleError)
+        )
         .subscribe(poke => {
           pokemon.push({
             name: poke.name,
@@ -22,12 +36,8 @@ export class PokemonService {
             types: poke.types
           })
         })
+      setTimeout(() => subscription.unsubscribe(), 1000)
     }
     return pokemon
   }
-}
-export interface aPokemon {
-  name: string,
-  id: number,
-  types: [{ type: { name: string } }]
 }
